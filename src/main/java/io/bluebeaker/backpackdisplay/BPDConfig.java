1deb43acfc8ff2f213daa818bd267cb38498fbcd
blue-beaker <yyy5566778899@126.com>
2024-10-31 21:15:41 +0800

    add default shulker boxes config

--- src/main/java/io/bluebeaker/backpackdisplay/BPDConfig.java
+++ src/main/java/io/bluebeaker/backpackdisplay/BPDConfig.java
@@ -9,5 +9,21 @@ import net.minecraftforge.common.config.Config.Type;
 public class BPDConfig {
     @Comment("Example")
     @LangKey("config.examplemod.example.name")
-    public static String[] displayRules = {};
+    public static String[] displayRules = {
+        "minecraft:white_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:orange_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:magenta_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:light_blue_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:yellow_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:lime_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:pink_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:gray_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:silver_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:cyan_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:purple_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:blue_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:brown_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:green_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:red_shulker_box#list#BlockEntityTag.Items",
+        "minecraft:black_shulker_box#list#BlockEntityTag.Items"};
 }
