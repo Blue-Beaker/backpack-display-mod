6221d5c01661014f205b45e0f9c0caa0f668a4be
blue-beaker <yyy5566778899@126.com>
2024-11-29 23:21:46 +0800

    clean code

--- src/main/java/io/bluebeaker/backpackdisplay/BPDTooltip.java
+++ src/main/java/io/bluebeaker/backpackdisplay/BPDTooltip.java
@@ -5,13 +5,10 @@ import java.util.List;
 import org.lwjgl.input.Keyboard;
 
 import io.bluebeaker.backpackdisplay.api.IDisplaySection;
-import io.bluebeaker.backpackdisplay.displayslot.IDisplaySlotEntry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.inventory.Slot;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.fml.client.config.GuiUtils;
@@ -116,67 +113,7 @@ public class BPDTooltip {
             }
         }
     }
-
-    /**
-     * Render item stack for the tooltip
-     * 
-     * @param stack Itemstack to render
-     * @param x
-     * @param y
-     */
-    public static void renderItemStack(ItemStack stack, int x, int y) {
-        client.getRenderItem().renderItemIntoGUI(stack, x, y);
-        String numRep = null;
-        if (stack.getCount() > 1)
-            numRep = getNumRepresentation(stack.getCount());
-        if (fontRenderer.getStringWidth(numRep) > 16) {
-            float scale = BPDConfig.label_scale;
-            drawLabelCorneredScaled(x, y, numRep, scale);
-            client.getRenderItem().renderItemOverlayIntoGUI(client.fontRenderer, stack, x, y, "");
-        } else {
-            client.getRenderItem().renderItemOverlayIntoGUI(client.fontRenderer, stack, x, y, numRep);
-        }
-    }
-
-    /**
-     * Draw a text in the Corner of slot
-     * 
-     * @param x
-     * @param y
-     * @param text
-     */
-    public static void drawLabelCorneredScaled(int x, int y, String text, float scale) {
-        GlStateManager.pushMatrix();
-        GlStateManager.scale(scale, scale, scale);
-        GlStateManager.disableLighting();
-        GlStateManager.disableDepth();
-        GlStateManager.disableBlend();
-        fontRenderer.drawStringWithShadow(text, (float) ((x + 15) / scale - fontRenderer.getStringWidth(text) + 2),
-                (float) ((y + 6 + fontRenderer.FONT_HEIGHT) / scale - fontRenderer.FONT_HEIGHT + 3), 16777215);
-        GlStateManager.enableLighting();
-        GlStateManager.enableDepth();
-        GlStateManager.enableBlend();
-        GlStateManager.popMatrix();
-    }
-
-    /**
-     * Draw a text in the middle of slot
-     * 
-     * @param x
-     * @param y
-     * @param text
-     */
-    public static void drawLabelCentered(int x, int y, String text) {
-        GlStateManager.disableLighting();
-        GlStateManager.disableDepth();
-        GlStateManager.disableBlend();
-        fontRenderer.drawStringWithShadow(text, (float) (x + 9 - fontRenderer.getStringWidth(text) / 2),
-                (float) (y + 9 - fontRenderer.FONT_HEIGHT / 2), 16777215);
-        GlStateManager.enableLighting();
-        GlStateManager.enableDepth();
-        GlStateManager.enableBlend();
-    }
-
+    /**Draws background for this tooltip */
     public static void drawBackground(int x, int y, int width, int height, int bgColor, int borderColorStart,
             int borderColorEnd) {
         final int zLevel = 300;
@@ -194,27 +131,4 @@ public class BPDTooltip {
                 borderColorEnd);
     }
 
-    public static String getNumRepresentation(int number) {
-        if (Math.log10(number) < BPDConfig.full_digits)
-            return String.valueOf(number);
-        if (number >= 1000000000) {
-            return String.format("%.1fG", number / 100000000 / 10.0);
-        } else if (number >= 100000000) {
-            return String.format("%dM", number / 1000000);
-        } else if (number >= 1000000) {
-            return String.format("%.1fM", number / 100000 / 10.0);
-        } else if (number >= 100000) {
-            return String.format("%dk", number / 1000);
-        } else if (number >= 1000) {
-            return String.format("%.1fk", number / 100 / 10.0);
-        } else {
-            return String.valueOf(number);
-        }
-    }
-
-    public static List<IDisplaySlotEntry> getRenderRules(ItemStack stack) {
-        Item item = stack.getItem();
-        List<IDisplaySlotEntry> entries = BPDRegistry.registry.get(item);
-        return entries;
-    }
 }
