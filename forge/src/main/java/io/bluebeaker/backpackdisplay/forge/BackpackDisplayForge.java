11710aa86745b1c6c6ed16411057b5394decf8ed
blue-beaker <yyy5566778899@126.com>
2025-01-18 19:11:53 +0800

    Register config screen for forge

--- forge/src/main/java/io/bluebeaker/backpackdisplay/forge/BackpackDisplayForge.java
+++ forge/src/main/java/io/bluebeaker/backpackdisplay/forge/BackpackDisplayForge.java
@@ -1,10 +1,14 @@
 package io.bluebeaker.backpackdisplay.forge;
 
 import dev.architectury.platform.forge.EventBuses;
+import io.bluebeaker.backpackdisplay.BPDConfig;
 import io.bluebeaker.backpackdisplay.BackpackDisplayMod;
 import io.bluebeaker.backpackdisplay.Keybind;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -15,6 +19,7 @@ public class BackpackDisplayForge {
         // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(BackpackDisplayMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         BackpackDisplayMod.init();
+        BackpackDisplayForge.registerConfigScreen();
     }
     @SubscribeEvent
     public void init(FMLCommonSetupEvent event){
@@ -22,5 +27,9 @@ public class BackpackDisplayForge {
     @SubscribeEvent
     public void registerBindings(RegisterKeyMappingsEvent event){
         event.register(Keybind.keyShowContents);
+    }
+
+    public static void registerConfigScreen() {
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> AutoConfig.getConfigScreen(BPDConfig.class, parent).get()));
     }
 }
